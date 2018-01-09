669b67e11f5b0e9418f558d73dd25cfb04f18257
fengmaoqiao <fm_qb@163.com>
2018-01-09 01:15:19 -0800

    添加了32位转64位接口代码

---
+++ 32Bto64B.v
@@ -0,0 +1,71 @@
+//**********************************************************
+//
+// Copyright(c) 2017 . Outwitcom Technology Co., LTD.
+// All rights reserved.
+//
+// ProjectName :
+// target      :
+// FileName    : 
+// Author      :fengmaoqiao 
+// E_mail      :fengmaoqiao@outwitcom.com 
+// Date        :
+// Version     :
+//
+// Modification history
+//--------------------------------------------------------
+// $Log:$
+// from the 32bit fifo into 64bit dinidma interface
+//
+//**********************************************************
+module(
+    //clock and reset
+    input               clk                                 ,
+    input               rst_n                               ,
+    //user port
+    input   [31:0]      s1_axis_fromhost_tdata              ,
+    input               s1_axis_fromhost_tvalid             ,
+    input   [3:0]       s1_axis_fromhost_tkeep              ,
+    input               s1_axis_fromhost_tlast              ,
+    output              s1_axis_fromhost_tready             ,
+
+    output  [63:0]      m1_axis_fromhost_tdata              ,
+    output              m1_axis_fromhost_tvalid             ,
+    output              m1_axis_fromhost_tlast              ,
+    output  [7:0]       m1_axis_fromhost_tkeep              ,
+    input               m1_axis_fromhost_tready             ,
+        
+      );
+
+//**********************************************************
+//Signal Declation
+//**********************************************************
+reg counter_data; 
+//**********************************************************
+//Main Code
+//**********************************************************
+always @(posedge clk or negedge rst_n)begin
+     if(!rst_n)begin
+        counter_data    <= 1'b0;    
+     end
+     else begin
+         if(s1_axis_fromhost_tlast)
+             counter_data <= 1'b0;
+         else begin
+            if(s1_axis_fromhost_tready && s1_axis_fromhost_tvalid)
+                counter_data <= counter_data + 1'b1;
+            else
+         end
+    end
+ end
+
+
+assign s1_axis_fromhost_tready = m1_axis_fromhost_tready;
+assign m1_axis_fromhost_tvalid = (counter_data == 1'b0) ? s1_axis_fromhost_tvalid : 1'b0;
+assign m1_axis_fromhost_tlast = s1_axis_fromhost_tlast;
+assign m1_axis_fromhost_tdata = (counter_data == 1'b1) ? {s1_axis_fromhost_tdata,32'b0} : {s1_axis_fromhost_temp,s1_axis_fromhost_data};
+assign m1_axis_fromhost_tkeep = (counter_data == 1'b1) ? 8'b11110000 : 8'b11111111;
+
+endmodule
+
+
+
